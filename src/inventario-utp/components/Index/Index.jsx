6418066d02b6744e0b13e9bf6f130f26d4401f9d
Daniel <danieljust2395@gmail.com>
2019-12-16 12:11:19 -0500

    welcome message alert

--- src/inventario-utp/components/Index/Index.jsx
+++ src/inventario-utp/components/Index/Index.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Card, CardHeader, Container, Row, Col, Alert } from "reactstrap";
-
+import "./Index.scss";
+import { useSelector } from "react-redux";
 import ProductList from "../ProductList/ProductList";
 const Index = props => {
+  const user = useSelector(({ auth }) => auth.user);
+  console.log("user", user);
+
+  const [alert, setAlert] = useState(false);
+  const showAlerts = () => {
+    if (user) {
+      setAlert(true);
+      setTimeout(() => {
+        setAlert(false);
+      }, 3000);
+    }
+  };
+
+  useEffect(() => {
+    showAlerts();
+  }, []);
   return (
     <React.Fragment>
       <div>
@@ -12,6 +29,12 @@ const Index = props => {
           <Row className="mt-5">
             <Col className="mb-5 mb-xl-0" xl="12">
               <Card className="shadow">
+                <Alert className="alert" color="success" isOpen={alert}>
+                  <p className="text-center">
+                    Bienvenido: {user && user.nombreUsuario}
+                  </p>
+                </Alert>
+
                 <CardHeader className="border-0">
                   <Row className="align-items-center">
                     <div className="col">
